98a153a75dffa4d66957d3bfdff638b18bbf41a0
Xin Zhang <zxaws@amazon.com>
2022-05-16 20:58:05 +0800

    Add basic character class.
    Add basic character class and implement player input.

---
+++ Source/DayOne/Character/GenericCharacter.cpp
@@ -0,0 +1,84 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GenericCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+AGenericCharacter::AGenericCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraArm"));
+	CameraArm->SetupAttachment(RootComponent);
+	CameraArm->TargetArmLength = 600.f;
+	CameraArm->bUsePawnControlRotation = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(CameraArm, USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false;
+}
+
+// Called when the game starts or when spawned
+void AGenericCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Process player input.
+void AGenericCharacter::OnMoveForward(float Value)
+{
+	if (Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AGenericCharacter::OnMoveRight(float Value)
+{
+	if (Controller != nullptr && Value != 0.f)
+	{
+		const FRotator YawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector Direction(FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y));
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AGenericCharacter::OnTurn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void AGenericCharacter::OnLookUp(float Value)
+{
+	AddControllerPitchInput(Value);
+}
+
+// Called every frame
+void AGenericCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AGenericCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (PlayerInputComponent)
+	{
+		PlayerInputComponent->BindAction("Jump", EInputEvent::IE_Pressed, this, &ThisClass::Jump);
+		PlayerInputComponent->BindAxis("MoveForward", this, &ThisClass::OnMoveForward);
+		PlayerInputComponent->BindAxis("MoveRight", this, &ThisClass::OnMoveRight);
+		PlayerInputComponent->BindAxis("Turn", this, &ThisClass::OnTurn);
+		PlayerInputComponent->BindAxis("LookUp", this, &ThisClass::OnLookUp);
+	}
+}
+
---
+++ Source/DayOne/Character/GenericCharacter.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "GenericCharacter.generated.h"
+
+UCLASS()
+class DAYONE_API AGenericCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AGenericCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+	// Process player input.
+	void OnMoveForward(float Value);
+	void OnMoveRight(float Value);
+	void OnTurn(float Value);
+	void OnLookUp(float Value);
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class USpringArmComponent* CameraArm;
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	class UCameraComponent* Camera;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
